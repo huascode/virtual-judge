f8b692fd8767a313bb837be02810368ed6a91a20
imaxct <imaxct@foxmail.com>
2016-10-22 16:28:52 +0800

    first add

---
+++ README.md
@@ -0,0 +1,5 @@
+virtual-judge
+=============
+
+复制自[原virtual-judge](https://github.com/chaoshxxu/virtual-judge/tree/9dc0be82ed7e05dc17b7f042a935bf3db8435ce4)
+最后一次有效提交
---
+++ src/remote/java/judge/remote/RemoteOj.java
@@ -0,0 +1,28 @@
+package judge.remote;
+
+
+public enum RemoteOj {
+
+    ACdream, //
+    Aizu, //
+    CodeForces, //
+    CSU, //
+    FZU, //
+    HDU, //
+    HUST, //
+    HYSBZ, //
+    LightOJ, //
+    NBUT, //
+    POJ, //
+    SCU, //
+    SGU, //
+    SPOJ, //
+    UESTC, //
+    UESTCOld, //
+    URAL, //
+    UVA, //
+    UVALive, //
+    ZOJ, //
+    ZTrening, //
+
+}
